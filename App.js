4881772032217efc493fd98d2f15619635780f60
kingBracko <weak1leaf@gmail.com>
2025-09-02 00:26:40 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Easthood Trendy!</h1>
+      <ul>
+  <li>Tshirt - 500KES</li>
+  <li>Jeans - 1000KES</li>
+  <li>Dress - 800KES</li>
+  
+</ul>
+    </div>
+  );
+}
+export default App;
